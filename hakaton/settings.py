96278dd621b54ddc58b5b0d467b88daf2a60f8dd
bipbip <ev3rgarden.off@gmail.com>
2024-07-25 06:39:31 +0000

    Fix

--- hakaton/settings.py
+++ hakaton/settings.py
@@ -1,5 +1,6 @@
 import os
 from celery.schedules import crontab
+
 """
 Django settings for hakaton project.
 
@@ -22,76 +23,101 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@#@gp@2=a0i1_7#5u5sod*xny$@!#@ii(4ebq@==2_d52&a$r)'
+SECRET_KEY = "django-insecure-@#@gp@2=a0i1_7#5u5sod*xny$@!#@ii(4ebq@==2_d52&a$r)"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ["backend.movie-rank.ru", "localhost:3000", "hakaton.movie-rank.ru"]
-CSRF_TRUSTED_ORIGINS =["https://backend.movie-rank.ru/*", "http://localhost:3000/*", "https://hakaton.movie-rank.ru/*"]
+ALLOWED_HOSTS = ["backend.movie-rank.ru", "localhost", "hakaton.movie-rank.ru"]
 
-CORS_ALLOWED_ORIGINS = ["backend.movie-rank.ru", "localhost:3000", "hakaton.movie-rank.ru"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://backend.movie-rank.ru",
+    "http://localhost:3000",
+    "https://hakaton.movie-rank.ru",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://backend.movie-rank.ru",
+    "http://localhost:3000",
+    "https://hakaton.movie-rank.ru",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SAMESITE = 'Lax'
 
-SESSION_COOKIE_AGE = 60*60*24
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
+
+SESSION_COOKIE_AGE = 60 * 60 * 24
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_DOMAIN = ".movie-rank.ru"  
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_DOMAIN = ".movie-rank.ru" 
+
+CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "corsheaders"
-    'project'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "project",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware"
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # Place CorsMiddleware before CommonMiddleware
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'hakaton.urls'
+
+ROOT_URLCONF = "hakaton.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'hakaton.wsgi.application'
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+WSGI_APPLICATION = "hakaton.wsgi.application"
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hakaton',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hakaton",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -101,16 +127,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -118,27 +144,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
-AUTH_USER_MODEL = 'project.CustomUser'
+AUTH_USER_MODEL = "project.CustomUser"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = '/var/www/hakaton/static/'
+STATIC_ROOT = "/var/www/hakaton/static/"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -151,18 +177,18 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # settings celery
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "UTC"
 
 
 CELERY_BEAT_SCHEDULE = {
-    'send-daily-recommendations-every-hour': {
-        'task': 'project.tasks.send_daily_recommendations_celery',
-        'schedule': crontab(minute=0, hour='*/1'), 
+    "send-daily-recommendations-every-hour": {
+        "task": "project.tasks.send_daily_recommendations_celery",
+        "schedule": crontab(minute=0, hour="*/1"),
     },
 }
