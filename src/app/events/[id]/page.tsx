d77826c3d2ee72576f1558079fe8d5c69724e1a6
krevetych <timurbyvalcev95@gmail.com>
2024-07-27 08:53:49 +0400

    Manual fixs

---
+++ src/app/events/[id]/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Event',
+	description: 'Страницы рекомендаций в видео лонгридов'
+}
+
+export default function EventPage() {
+	return <div>Hello</div>
+}
